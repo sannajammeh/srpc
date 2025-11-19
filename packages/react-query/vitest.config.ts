f99378d79ba7151cc9ca04703a37596d1ad8d252
sannajammeh <sannajam@outlook.com>
2025-11-19 20:12:58 +0100

    fix: reset version to 0.0.0 for core and react-query packages; update tsconfig and vitest config

--- packages/react-query/vitest.config.ts
+++ packages/react-query/vitest.config.ts
@@ -3,11 +3,5 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    globalSetup: "./src/test/globalSetup.ts",
-    typecheck: {
-      enabled: true,
-      checker: "tsc",
-    },
-    testTimeout: 30 * 1000,
   },
 });
